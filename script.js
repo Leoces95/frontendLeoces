24d18884b2e295e3960a786d01afa7499f74e27c
Leoces <leocesvives@gmail.com>
2024-08-03 18:06:24 -0500

    Se agrega el esquema inicial de la p+agina web que comprende el protafolio de Leonel Antonio Martínez

---
+++ script.js
@@ -0,0 +1,9 @@
+function perfil() {
+    alert('Persona entusiasta, autodidacta con capacidad de adaptación y obediencia');
+}
+function habilidades(){
+    alert('Scrum, escucha activa, comunicación asertiva')
+}
+function idiomas(){
+    alert('English middlevel')
+}
