5fe46bcfac3521b82f0632cdca408e198e2de265
Leoces <leocesvives@gmail.com>
2024-08-13 14:25:31 -0500

    Se continua con el proyecto frontend, en esta ocasión se crea la página web para el perfil de Leonel Antonio Martínez, se modifica el html, el css y el js con el objetivo de pulir el portafolio del autor

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 function perfil() {
-    alert('Persona entusiasta, autodidacta con capacidad de adaptación y obediencia');
+    const perfil = document.getElementById('mostrarPerfil');
+    if(perfil.hidden){
+        perfil.hidden = false;
+    }
+    else{
+        perfil.hidden = true;
+    }
+    alert('Entusiasta, autodidacta, adaptación, obediencia');
 }
 function habilidades(){
     alert('Scrum, escucha activa, comunicación asertiva')
 }
 function idiomas(){
     alert('English middlevel')
+}
+function contacto(){
+    alert('Leo contact info')
 }
